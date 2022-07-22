2c4976acf2ad8ac294e4b20a031ce08f35b102e9
Sebastian Pineda <63763483+sebascamilo12@users.noreply.github.com>
2022-07-22 15:26:30 -0500

    Se crea UserRepository

---
+++ domain/model/src/main/java/co/com/sofkau/model/user/gateways/UserRepository.java
@@ -0,0 +1,15 @@
+package co.com.sofkau.model.user.gateways;
+
+
+import co.com.sofkau.model.user.User;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository {
+
+    Mono<User> save(User user);
+
+    Mono<User> findById(String id);
+
+    Flux<User> findAll();
+}
