f5081f87323ea1717878a79b08282ad230e2d35a
Sebastian Pineda <63763483+sebascamilo12@users.noreply.github.com>
2022-07-22 15:48:30 -0500

    Se corrige FindAllUsersUseCase

--- domain/usecase/src/main/java/co/com/sofkau/usecase/user/FindAllUsersUseCase.java
+++ domain/usecase/src/main/java/co/com/sofkau/usecase/user/FindAllUsersUseCase.java
@@ -2,13 +2,16 @@ package co.com.sofkau.usecase.user;
 
 import co.com.sofkau.model.user.User;
 import co.com.sofkau.model.user.gateways.UserRepository;
+import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 
 import java.util.function.Supplier;
 
+
+@RequiredArgsConstructor
 public class FindAllUsersUseCase implements Supplier<Flux<User>> {
 
-    private static UserRepository userRepository;
+    private final UserRepository userRepository;
     @Override
     public Flux<User> get() {
         return this.userRepository.findAll();
