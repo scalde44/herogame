51906c63b13c6ec276ce66451508180426e7f4b5
JosueArdila <42660657+JosueArdila@users.noreply.github.com>
2022-07-22 23:34:55 -0500

    Se implementan las entidades

--- domain/model/src/main/java/co/com/sofkau/model/game/Round.java
+++ domain/model/src/main/java/co/com/sofkau/model/game/Round.java
@@ -1,4 +1,45 @@
 package co.com.sofkau.model.game;
 
-public class Round {
+import co.com.sofkau.model.game.identities.PlayerId;
+import co.com.sofkau.model.game.identities.RoundId;
+import co.com.sofkau.model.game.values.RoundNumber;
+import co.com.sofkau.model.generic.Entity;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Round extends Entity<RoundId> {
+
+    private Set<PlayerId> playerIds;
+    private RoundNumber roundNumber;
+    private Boolean isFinished;
+
+    public Round(RoundId entityId, RoundNumber roundNumber) {
+        super(entityId);
+        this.roundNumber = roundNumber;
+        this.playerIds = new HashSet<>();
+        this.isFinished = Boolean.FALSE;
+    }
+
+    public Set<PlayerId> playerIds() {
+        return playerIds;
+    }
+
+    public RoundNumber roundNumber() {
+        return roundNumber;
+    }
+
+    public Boolean isFinished() {
+        return isFinished;
+    }
+
+    public void finish() {
+        this.isFinished = Boolean.TRUE;
+    }
+
+    public void addPlayer(PlayerId playerId) {
+        this.playerIds.add(Objects.requireNonNull(playerId));
+    }
+
 }
