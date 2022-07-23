19ddd23fc69c2c262063c3fc2ec56bc83567d197
Steven Calderon <scalderon1504@gmail.com>
2022-07-23 15:40:59 -0500

    Se implementan las interfaces genericas para almacenar eventos

---
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/sofkau/mongo/helper/MongoEventStoreRepository.java
@@ -0,0 +1,35 @@
+package co.com.sofkau.mongo.helper;
+
+import co.com.sofkau.model.generic.DomainEvent;
+import co.com.sofkau.model.generic.EventStoreRepository;
+import co.com.sofkau.model.generic.StoredEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class MongoEventStoreRepository implements EventStoreRepository {
+
+    private final ReactiveMongoTemplate mongoTemplate;
+    private final StoredEvent.EventSerializer eventSerializer;
+
+    @Override
+    public Flux<DomainEvent> getEventsBy(String aggregateName, String aggregateRootId) {
+        var query = new Query(Criteria.where("aggregateRootId").is(aggregateRootId));
+        return mongoTemplate.find(query, DocumentEventStored.class, aggregateName)
+                .map((documentEventStored -> documentEventStored.getStoredEvent().deserializeEvent(eventSerializer)));
+    }
+
+    @Override
+    public Mono<Void> saveEvent(String aggregateName, String aggregateRootId, StoredEvent storedEvent) {
+        var eventStored = new DocumentEventStored();
+        eventStored.setAggregateRootId(aggregateRootId);
+        eventStored.setStoredEvent(storedEvent);
+        return mongoTemplate.save(eventStored, aggregateName).then();
+    }
+}
