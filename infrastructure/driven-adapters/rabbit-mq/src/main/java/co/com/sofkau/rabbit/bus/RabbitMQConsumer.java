8e77e9f511b87a3727078b2cf02cd2f58acb8ee4
Steven Calderon <scalderon1504@gmail.com>
2022-07-25 01:04:07 -0500

    Implementacion de rabbit final con listener

--- infrastructure/driven-adapters/rabbit-mq/src/main/java/co/com/sofkau/rabbit/bus/RabbitMQConsumer.java
+++ infrastructure/driven-adapters/rabbit-mq/src/main/java/co/com/sofkau/rabbit/bus/RabbitMQConsumer.java
@@ -1,6 +1,7 @@
 package co.com.sofkau.rabbit.bus;
 
 import co.com.sofkau.rabbit.generic.SuccessNotificationSerializer;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
@@ -9,18 +10,23 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class RabbitMQConsumer {
+    private final EventListenerSubscriber eventListenerSubscriber;
 
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue(value = "${queues.game.name}", durable = "true"),
             exchange = @Exchange(value = "${queues.exchange}", type = "topic"),
             key = "${queues.game.routing.key}"
     ))
-    public void recievedMessageJuego(Message<String> message) {
+    public void recievedMessageJuego(Message<String> message) throws IOException {
         var successNotification = SuccessNotificationSerializer.instance().deserialize(message.getPayload());
         var event = successNotification.deserializeEvent();
         log.info("Llego este evento: {}", event);
+        this.eventListenerSubscriber.onNext(event);
     }
 }
--- infrastructure/driven-adapters/rabbit-mq/src/main/java/co/com/sofkau/rabbit/config/EventListenerConfig.java
+++ infrastructure/driven-adapters/rabbit-mq/src/main/java/co/com/sofkau/rabbit/config/EventListenerConfig.java
@@ -1,2 +1,27 @@
-package co.com.sofkau.rabbit.config;public class EventListenerConfig {
+package co.com.sofkau.rabbit.config;
+
+import co.com.sofkau.model.game.events.CreatedRound;
+import co.com.sofkau.model.game.events.GameStarted;
+import co.com.sofkau.usecase.game.AssignRoundPlayersUseCase;
+import co.com.sofkau.usecase.game.CreateRoundUseCase;
+import co.com.sofkau.usecase.game.DistributeCardsUseCase;
+import co.com.sofkau.usecase.generic.UseCaseWrap;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
+
+import java.util.Set;
+
+@Configuration
+public class EventListenerConfig {
+    @Bean
+    public Set<UseCaseWrap> useCasesForListener(CreateRoundUseCase createRoundUseCase,
+                                                 DistributeCardsUseCase distributeCardsUseCase,
+                                                 AssignRoundPlayersUseCase assignRoundPlayersUseCase) {
+        return Set.of(
+                new UseCaseWrap(createRoundUseCase, GameStarted.EVENT_TYPE),
+                new UseCaseWrap(distributeCardsUseCase, GameStarted.EVENT_TYPE),
+                new UseCaseWrap(assignRoundPlayersUseCase, CreatedRound.EVENT_TYPE)
+        );
+    }
 }
