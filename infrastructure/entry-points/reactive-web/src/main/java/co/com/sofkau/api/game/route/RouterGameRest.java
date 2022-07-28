b48637191dfda9a7430af8fca30a3d5a2b7c8452
Sebastian Pineda <63763483+sebascamilo12@users.noreply.github.com>
2022-07-27 23:29:43 -0500

    Se crea servicio para obtener todos los eventos

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/sofkau/api/game/route/RouterGameRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/sofkau/api/game/route/RouterGameRest.java
@@ -1,14 +1,12 @@
 package co.com.sofkau.api.game.route;
 
-import co.com.sofkau.api.game.handler.AddCardToBoardHandler;
-import co.com.sofkau.api.game.handler.CreateGameHandler;
-import co.com.sofkau.api.game.handler.FinishRoundHandler;
-import co.com.sofkau.api.game.handler.StartGameHandler;
+import co.com.sofkau.api.game.handler.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
@@ -33,5 +31,9 @@ public class RouterGameRest {
     @Bean
     public RouterFunction<ServerResponse> routerFinishRoundFunction(FinishRoundHandler handler) {
         return route(POST("/api/v1/game/round/finish"), handler::finish);
+    }
+    @Bean
+    public RouterFunction<ServerResponse> routerGetEventsFunction(GetEventsHandler handler){
+        return route(GET("/api/v1/game/events/{gameId}"), handler::getEvents);
     }
 }
